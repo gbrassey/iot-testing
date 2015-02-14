c9c4bba1acb3a8e1d866babfc00970255fdbb45c
George Brassey <gbrassey@gmail.com>
2015-02-14 12:22:55 -0500

    added gulp environment and watch sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,19 @@ var config = {
 	bowerDir: './bower_components',
 	sassDir: './resources/sass',
 	cssDir: './public/stylesheets',
-	jsDir: './public/javascripts'
+	jsDir: './public/javascripts',
+	production: true
+
 };
 
 gulp.task('css', function() { 
     return sass(config.sassDir + '/style.scss', {
-    		style: 'compressed',
+			style: (function() {
+				if (config.production)
+					return 'compressed';
+				else
+					return 'expanded';
+			})(),
 			loadPath: [
 				'./resources/sass',
 				config.bowerDir + '/bootstrap-sass-official/assets/stylesheets'
@@ -19,7 +26,14 @@ gulp.task('css', function() { 
 		.on('error', function (err) {
 			console.error('Error!', err.message);
 		})
-        .pipe(gulp.dest(config.cssDir)); 
+		.pipe(gulp.dest(config.cssDir)); 
+});
+
+gulp.task('set-dev', function() {config.production = false});
+ gulp.task('watch-sass', function() {
+     gulp.watch(config.sassDir + '/**/*.scss', ['css']); 
 });
 
   gulp.task('default', ['css']);
+gulp.task('dev', ['set-dev', 'css']);
+gulp.task('watch', ['dev', 'watch-sass']);
