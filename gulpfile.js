f3d898ceb5fd5dd3213974c4d12f60969e76dfa6
George Brassey <gbrassey@gmail.com>
2015-02-15 10:02:36 -0500

    bootstrapped index page

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	usemin = require('gulp-usemin'),
 	uglify = require('gulp-uglify'),
+	autoprefixer = require('autoprefixer'),
 	gulpFilter = require('gulp-filter');
 
 var config = {
@@ -40,7 +41,7 @@ gulp.task('scripts', function() {
 
 		gulp.src(config.viewSrc + '/**/*.ejs')
 			.pipe(usemin({
-				js: [uglify(), gulp.dest('./public')],
+				js: [uglify(), autoprefixer(), gulp.dest('./public')],
 				assetsDir: '.'
 			}))
 			.pipe(vwFilter)
